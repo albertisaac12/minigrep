4d2b726dcd6b91751ce34517af5e8f9d98f6b497
Abhi <abhiodf9@gmail.com>
2025-07-26 15:52:12 +0530

    Completed minigrep

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args = env::args().collect::<Vec<String>>();
     // dbg!(args);
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
     
@@ -15,10 +15,11 @@ fn main() {
     println!("In file {}",config.file_path);
 
    if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
    }
     
 }
+
 
 
